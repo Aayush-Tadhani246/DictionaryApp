ef26a340840ab7c628a2a81718a68308384928a5
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2021-07-18 09:38:54 -0400

    Update App.js

--- App.js
+++ App.js
@@ -3,16 +3,7 @@ import HomeScreen from './Components/HomeScreen';
 import { View } from 'react-native';
 
 export default class App extends React.Component {
-  constructor(){
-    super()
-    this.state={
-      word:' ', 
-      definition:' ', 
-      lexicalCategory:' ',
-      text:'',
-      displayText:''
-    }
-  }
+  
   render(){
   return (
     <View>
